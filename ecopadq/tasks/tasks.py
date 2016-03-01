67c70cad48f5f9f5fc438fa88c333857269dc7a0
SWC Learner <ecomodeller@gmail.com>
2016-03-01 13:53:27 -0600

    modify tasks

--- ecopadq/tasks/tasks.py
+++ ecopadq/tasks/tasks.py
@@ -16,3 +16,18 @@ def add(x, y):
     """
     result = x + y
     return result
+
+def teco_spruce_setup(pars):
+    """ Setup task convert parameters from html portal
+	to file, and store the file in input folder.
+	Then call teco_spruce_run()
+    """
+
+def teco_spruce_run(pars,forcing,obs,output,MCMC):
+    """ Run task compile teco_spruce fortran code
+	args: pars is parameters from portal
+	      forcing is climate forcing input
+	      obs is observation data for DA
+	      output is model output folder
+	      MCMC is flag for DA(1) or simulation(0)
+    """
