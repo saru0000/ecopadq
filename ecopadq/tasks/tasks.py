1e1afc7d86650a137fb74209ef14d7c0a9307508
Mark Stacy <mbstacy@gmail.com>
2016-05-01 19:34:57 -0500

    added report for simulation and forecast tasks

--- ecopadq/tasks/tasks.py
+++ ecopadq/tasks/tasks.py
@@ -2,7 +2,7 @@ from celery.task import task
 from dockertask import docker_task
 from subprocess import call,STDOUT
 from jinja2 import Template
-from shutil import copyfile
+from shutil import copyfile, move
 import requests,os
 
 #Default base directory 
@@ -42,7 +42,21 @@ def teco_spruce_simulation(pars): # ,model_type="0", da_params=None):
     docker_opts = "-v {0}:/usr/local/src/myscripts/graphoutput:z ".format(host_data_resultDir)
     docker_cmd = None
     result = docker_task(docker_name="ecopad_r",docker_opts=docker_opts,docker_command=docker_cmd,id=task_id)
-    return "http://{0}/ecopad_tasks/{1}".format(result['host'],result['task_id']) 
+    #Clean up result Directory
+    clean_up(resultDir)
+    #Create Report
+    report_data ={'zero_label':'GPP','zero_url':'/ecopad_tasks/{1}/plot/{2}'.format(task_id,'gpp.png'),
+                'one_label':'ER','one_url':'/ecopad_tasks/{1}/plot/{2}'.format(task_id,'er.png'),
+                'two_label':'Foliage','two_url':'/ecopad_tasks/{1}/plot/{2}'.format(task_id,'foliage.png'),
+                'three_label':'Wood','three_url':'/ecopad_tasks/{1}/plot/{2}'.format(task_id,'wood.png'),
+                'four_label':'Root','four_url':'/ecopad_tasks/{1}/plot/{2}'.format(task_id,'root.png'),
+                'five_label':'Soil','five_url':'/ecopad_tasks/{1}/plot/{2}'.format(task_id,'soil.png')}
+    report_data['title']="SPRUCE Ecological Simulation Task Report"
+    report_data['description']="Simulations of carbon fluxes and pool sizes for SPRUCE experiment based on user defined initial parameters."
+
+    report = create_report('report',report_data)
+    return {"data":"http://{0}/ecopad_tasks/{1}".format(result['host'],result['task_id']),
+            "report": "http://{0}/ecopad_tasks/{1}/{2}".format(result['host'],result['task_id'],report)} 
   
 @task()
 def teco_spruce_data_assimilation(pars):
@@ -85,21 +99,23 @@ def teco_spruce_forecast(pars,forecast_year,forecast_day,temperature_treatment=0
     task_id = str(teco_spruce_forecast.request.id)
     resultDir = setup_result_directory(task_id)
     param_filename = create_template('SPRUCE_pars',pars,resultDir,check_params)
-    da_param_filename = create_template('SPRUCE_da_pars',pars,resultDir,check_params)
+    #da_param_filename = create_template('SPRUCE_da_pars',pars,resultDir,check_params)
     #Set Param estimation file from DA 
     if not da_task_id:
         da_task_id = "default"
-    try:
-        copyfile("{0}/ecopad_tasks/{1}/Paraest.txt".format(basedir,da_task_id),"{0}/Paraest.txt".format(resultDir))
-    except:
-        raise("Parameter Estimation file location problem. {0} file not found.".format("{0}/ecopad_tasks/{1}/Paraest.txt".format(basedir,da_task_id)))
-    #SPRUCE_da_pars
-    #da_param_filename = create_template('SPRUCE_da_pars',da_params,resultDir,check_params)
-    #if da_params:
-    #    da_param_filename = create_template('SPRUCE_da_pars',da_params,resultDir,check_params)
-    #else:
-    #    copyfile("{0}/ecopad_tasks/default/SPRUCE_da_pars.txt".format(basedir),"{0}/SPRUCE_da_pars.txt".format(resultDir))
-    #    da_param_filename ="SPRUCE_da_pars.txt"
+        try:
+            copyfile("{0}/ecopad_tasks/{1}/Paraest.txt".format(basedir,da_task_id),"{0}/Paraest.txt".format(resultDir))
+            copyfile("{0}/ecopad_tasks/{1}/SPRUCE_da_pars.txt".format(basedir,da_task_id),"{0}/SPRUCE_da_pars.txt".format(resultDir))
+        except:
+            error_file = "{0}/ecopad_tasks/{1}/Paraest.txt or SPRUCE_da_pars.txt".format(basedir,da_task_id)
+            raise("Parameter Estimation file location problem. {0} file not found.".format(error_file))
+    else:
+        try:
+            copyfile("{0}/ecopad_tasks/{1}/input/Paraest.txt".format(basedir,da_task_id),"{0}/Paraest.txt".format(resultDir))
+            copyfile("{0}/ecopad_tasks/{1}/input/SPRUCE_da_pars.txt".format(basedir,da_task_id),"{0}/SPRUCE_da_pars.txt".format(resultDir))
+        except:
+            error_file = "{0}/ecopad_tasks/{1}/input/Paraest.txt or SPRUCE_da_pars.txt".format(basedir,da_task_id)
+            raise("Parameter Estimation file location problem. {0} file not found.".format(error_file))
     #Run Spruce TECO code
     host_data_resultDir = "{0}/ecopad_tasks/{1}".format(host_data_dir,task_id)
     docker_opts = "-v {0}:/data:z ".format(host_data_resultDir)
@@ -113,7 +129,36 @@ def teco_spruce_forecast(pars,forecast_year,forecast_day,temperature_treatment=0
     docker_opts = "-v {0}:/data:z ".format(host_data_resultDir)
     docker_cmd ="Rscript ECOPAD_forecast_viz.R {0} {1} {2} {3}".format("obs_file/SPRUCE_obs.txt","/data","/data",100)
     result = docker_task(docker_name="ecopad_r",docker_opts=docker_opts,docker_command=docker_cmd,id=task_id)
-    return "http://{0}/ecopad_tasks/{1}".format(result['host'],result['task_id'])
+    #Clean up result Directory
+    clean_up(resultDir)
+    #Create Report
+    report_data ={'zero_label':'GPP Forecast','zero_url':'/ecopad_tasks/{1}/plot/{2}'.format(task_id,'gpp_forecast.png'),
+                'one_label':'ER Forecast','one_url':'/ecopad_tasks/{1}/plot/{2}'.format(task_id,'er_forecast.png'),
+                'two_label':'Foliage Forecast','two_url':'/ecopad_tasks/{1}/plot/{2}'.format(task_id,'foliage_forecast.png'),
+                'three_label':'Wood Forecast','three_url':'/ecopad_tasks/{1}/plot/{2}'.format(task_id,'wood_forecast.png'),
+                'four_label':'Root Forecast','four_url':'/ecopad_tasks/{1}/plot/{2}'.format(task_id,'root_forecast.png'),
+                'five_label':'Soil Forecast','five_url':'/ecopad_tasks/{1}/plot/{2}'.format(task_id,'soil_forecast.png')}
+    report_data['title']="SPRUCE Ecological Forecast Task Report"
+    desc = "Use constrained parameters from Data Assimilation to predict carbon fluxes and pool sizes. "
+    desc = desc + "Forcing inputs are genereated by auto-regression model using historical climate data of the SPRUCE site. "
+    desc = desc + "Allow users to choose which year and day to make predictations of ecosystem in response to treatment effects."
+    report_data['description']=desc
+    report = create_report('report',report_data)
+    return {"data":"http://{0}/ecopad_tasks/{1}".format(result['host'],result['task_id']),
+            "report": "http://{0}/ecopad_tasks/{1}/{2}".format(result['host'],result['task_id'],report)}
+
+def clean_up(resultDir):
+    move("{0}/SPRUCE_pars.txt".format(resultDir),"{0}/input/SPRUCE_pars.txt".format(resultDir))
+    move("{0}/SPRUCE_yearly.txt".format(resultDir),"{0}/output/SPRUCE_yearly.txt".format(resultDir))
+    for mvfile in glob("{0}/Simu_dailyflux*.txt".format(resultDir):
+        move(mvfile, "{0}/output".format(resultDir))
+    for mvfile in glob("{0}/*.png".format(resultDir):
+        move(mvfile, "{0}/plot".format(resultDir))
+    try:
+        move("{0}/SPRUCE_da_pars.txt".format(resultDir),"{0}/input/SPRUCE_da_pars.txt".format(resultDir))
+        move("{0}/Paraest.txt".format(resultDir),"{0}/input/Paraest.txt".format(resultDir))
+    except:
+        pass
 
 def create_template(tmpl_name,params,resultDir,check_function):
     tmpl = os.path.join(os.path.dirname(__file__),'templates/{0}.tmpl'.format(tmpl_name))
@@ -124,9 +169,21 @@ def create_template(tmpl_name,params,resultDir,check_function):
         f2.write(template.render(check_function(params)))
     return '{0}.txt'.format(tmpl_name)
 
+def create_report(tmpl_name,data):
+    tmpl = os.path.join(os.path.dirname(__file__),'templates/{0}.tmpl'.format(tmpl_name))
+    with open(tmpl,'r') as f:
+        template=Template(f.read())
+    report_file = os.path.join(resultDir,'{0}.html'.format(tmpl_name))
+    with open(report_file,'w') as f2:
+        f2.write(template.render(data))
+    return '{0}.html'.format(tmpl_name)
+
 def setup_result_directory(task_id):
     resultDir = os.path.join(basedir, 'ecopad_tasks/', task_id)
     os.makedirs(resultDir)
+    os.makedirs("{0}/input".format(resultDir))
+    os.makedirs("{0}/output".format(resultDir))
+    os.makedirs("{0}/plot".format(resultDir))
     return resultDir 
 
 def check_params(pars):
